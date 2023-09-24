afaa650ab1ad5493ba3f7206889bf662fea4766f
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-09-24 12:02:04 -0700

    feat(variables): make team slug optional

--- data.tf
+++ data.tf
@@ -1,3 +1,5 @@
 data "github_team" "self" {
+  count = var.team_slug != null ? 1 : 0
+
   slug = var.team_slug
 }
--- team.tf
+++ team.tf
@@ -1,5 +1,7 @@
 resource "github_team_repository" "self" {
+  count = var.team_slug != null ? 1 : 0
+
   permission = "admin"
   repository = github_repository.self.name
-  team_id    = data.github_team.self.id
+  team_id    = data.github_team.self[0].id
 }
