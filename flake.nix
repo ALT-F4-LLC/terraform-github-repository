c92fd73c2ab477252600bc5fd4f26915873eb38f
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2024-02-28 12:19:02 -0800

    Update provider to 6.0.0 (#15)
    * feat(flake): update github provider
    
    * chore(flake): update provider to 6.0.0
    
    * fix(terraformignore)
    
    * fix(justfile)
    
    * docs(README)

--- flake.nix
+++ flake.nix
@@ -3,53 +3,61 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = inputs@{ flake-parts, nixpkgs, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }:
-        let
-          inherit (pkgs) just terraform terraform-docs;
-          terraform-github = pkgs.terraform-providers.mkProvider {
+  outputs = inputs @ {
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        inherit (pkgs) just terraform-docs;
+        terraform = pkgs.terraform.withPlugins (p: [
+          (pkgs.terraform-providers.mkProvider {
+            hash = "sha256-y8DMpNSySMbe7E+sGVQcQdEyulq4Wnp5ryYD7FQO/fc=";
             homepage = "https://registry.terraform.io/providers/integrations/github";
             owner = "integrations";
             repo = "terraform-provider-github";
-            rev = "v5.41.0";
-            hash = "sha256-fWxWcHy3TWFEpkU9uDSsHbUu+dnQKfvquN63eZH+Qf0=";
+            rev = "v6.0.0";
             vendorHash = null;
-          };
-        in
-        {
-          _module.args.pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          };
+          })
+        ]);
+      in {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
 
-          packages = {
-            terraform-with-plugins = pkgs.terraform.withPlugins (ps: [
-              terraform-github
-            ]);
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [
+              just
+              terraform
+              terraform-docs
+            ];
+          };
+        };
 
-            default = pkgs.runCommand "default"
-              {
-                src = ./.;
-              } ''
+        packages = {
+          default =
+            pkgs.runCommand "default"
+            {
+              src = ./.;
+            } ''
               mkdir -p $out
               cp -R $src/*.tf $out
 
-              ${config.packages.terraform-with-plugins}/bin/terraform -chdir="$out" init
-              ${config.packages.terraform-with-plugins}/bin/terraform -chdir="$out" validate
+              ${terraform}/bin/terraform -chdir="$out" init
+              ${terraform}/bin/terraform -chdir="$out" validate
             '';
-          };
-
-          devShells = {
-            default = pkgs.mkShell {
-              buildInputs = [
-                just
-                terraform
-                terraform-docs
-              ];
-            };
-          };
         };
+      };
     };
 }
--- justfile
+++ justfile
@@ -1,7 +1,33 @@
+_default:
+    just --list
+
+build:
+    nix build --json --no-link --print-build-logs
+
+cache-build cache_name="altf4llc-os":
+    just build \
+        | jq -r '.[].outputs | to_entries[].value' \
+        | cachix push {{ cache_name }}
+
+cache-inputs cache_name="altf4llc-os":
+    nix flake archive --json \
+      | jq -r '.path,(.inputs|to_entries[].value.path)' \
+      | cachix push "{{ cache_name }}"
+
+cache-shell cache_name="altf4llc-os":
+    nix develop --profile "dev-profile" -c true
+    cachix push "{{ cache_name }}" "dev-profile"
+
 check:
-  nix build --json --no-link --print-build-logs
+    nix flake check
 
 docs:
 	terraform-docs markdown table \
 		--output-file README.md \
 		--output-mode inject .
+
+init:
+    terraform init
+
+validate:
+    terraform validate
